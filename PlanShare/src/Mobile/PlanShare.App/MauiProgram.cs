fda43d81ee6aa720553be95d18f4c334fdcd6377
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2026-09-15 11:46:07 -0300

    Alterando fontes para os textos

--- PlanShare/src/Mobile/PlanShare.App/MauiProgram.cs
+++ PlanShare/src/Mobile/PlanShare.App/MauiProgram.cs
@@ -12,9 +12,10 @@ public static class MauiProgram
                 fonts.AddFont("Raleway-Black.ttf", "RalewayBlack");
                 fonts.AddFont("Raleway-Regular.ttf", "RalewayRegular");
                 fonts.AddFont("Raleway-Thin.ttf", "RalewayThin");
+                fonts.AddFont("WorkSans-Black.ttf", "WorkSansBlack");
                 fonts.AddFont("WorkSans-Regular.ttf", "WorkSansRegular");
-                fonts.AddFont("WorkSans-Bold.ttf", "WorkSansBold");
             });
+
 
         return builder.Build();
     }
